d43dfb5c098ff2112a9c664c496525a872a40004
lucas <allard.lucas.97@gmail.com>
2025-11-24 18:26:36 +0100

    feat: configuration shadcn/ui et dépendances UI
    - Installation et configuration shadcn/ui
    - Ajout fonction utilitaire cn() pour merge classes Tailwind
    - Installation dépendances: class-variance-authority, clsx, tailwind-merge
    - Downgrade zod vers 3.25 pour compatibilité TanStack Form

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,2 +1,9 @@
-// Utility functions will be defined here
-export {}
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Merge Tailwind CSS classes with clsx
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
