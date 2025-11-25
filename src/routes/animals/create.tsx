bdec2fb21f96f1a88e52d3fac1f1537a46b1c84b
lucas <allard.lucas.97@gmail.com>
2025-11-25 11:28:39 +0100

    feat: intégration GameView dans page index avec onglets
    - Page index avec AnimalTabs et AnimalGameView
    - Sélection automatique premier animal vivant
    - Redirection vers création si aucun animal
    - Mise à jour routes et styles

--- src/routes/animals/create.tsx
+++ src/routes/animals/create.tsx
@@ -6,14 +6,5 @@ export const Route = createFileRoute('/animals/create')({
 })
 
 function CreateAnimalComponent() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="font-pixel text-3xl text-text-primary mb-8 text-center">
-          Créer un nouvel animal
-        </h1>
-        <CreateAnimalForm />
-      </div>
-    </div>
-  )
+  return <CreateAnimalForm />
 }
